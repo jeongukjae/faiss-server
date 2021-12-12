374688f4ec26fc666bbc2f482ba8a3990edd30a0
Jeong Ukjae <jeongukjae@gmail.com>
2021-12-12 23:26:08 +0900

    update default server endpoint

--- main.go
+++ main.go
@@ -9,9 +9,9 @@ import (
 var (
 	// command-line options:
 	// gRPC server endpoint
-	grpcServerEndpoint = flag.String("grpc_server_endpoint", "localhost:8000", "gRPC server endpoint")
+	grpcServerEndpoint = flag.String("grpc_server_endpoint", "0.0.0.0:8000", "gRPC server endpoint")
 	// http server endpoint
-	httpServerEndpoint = flag.String("http_server_endpoint", "localhost:8001", "http server endpoint")
+	httpServerEndpoint = flag.String("http_server_endpoint", "0.0.0.0:8001", "http server endpoint")
 	// faiss index path
 	faissIndexPath = flag.String("faiss_index", "", "path of prebuilt faiss index")
 )
